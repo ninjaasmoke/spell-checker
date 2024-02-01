42de86d82b421f57550c752331a682a4b8562f2e
for <nithins674@gmail.com>
2024-02-01 20:52:24 +0000

    added code for levelshtein distance

---
+++ include/levenshtein.h
@@ -0,0 +1,10 @@
+// levenshtein.h : Nithin Sai
+// desc : Header file for levenshtein.cpp
+
+#ifndef LEVENSHTEIN_H
+#define LEVENSHTEIN_H
+
+#include <string>
+int calculateLevenshteinDistance(const std::string& str1, const std::string& str2);
+
+#endif // LEVENSHTEIN_H
---
+++ src/levelshtein.cpp
@@ -0,0 +1,34 @@
+// levenshtein.cpp : Nithin Sai
+// desc : Implementation file for levenshtein.h
+
+#include "levenshtein.h"
+
+int min(int a, int b, int c)
+{
+    return std::min(std::min(a, b), c);
+}
+
+int calculateLevenshteinDistance(const std::string &str1, const std::string &str2)
+{
+    const int len1 = str1.length();
+    const int len2 = str2.length();
+
+    int distances[len1 + 1][len2 + 1];
+
+    for (int i = 0; i <= len1; ++i)
+    {
+        for (int j = 0; j <= len2; ++j)
+        {
+            if (i == 0)
+                distances[i][j] = j;
+            else if (j == 0)
+                distances[i][j] = i;
+            else if (str1[i - 1] == str2[j - 1])
+                distances[i][j] = distances[i - 1][j - 1];
+            else
+                distances[i][j] = 1 + min(distances[i - 1][j], distances[i][j - 1], distances[i - 1][j - 1]);
+        }
+    }
+
+    return distances[len1][len2];
+}
